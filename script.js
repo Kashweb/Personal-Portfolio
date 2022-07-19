de0fa313a2f03baf59c095e6f7bda84553f9e2e3
Kashish Chawla <69769135+kashishchawla1@users.noreply.github.com>
2022-07-19 22:23:52 +0930

    Responsive Navbar

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-$(document).ready(function()){
+// $(document).ready(function()){
+//     $(window).scroll(function(){
+//         if(this.scrollY > 10){
+//             $(".navnbar").addClass("scrolledNavbar");
+//         }
+//     }
+// });
+
+$(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrollY > 10){
-            $(".navnbar").addClass("scrolledNavbar");
+        if(window.scrollY > 100){
+            $(".navbar").addClass("scrolledNavbar");
+        }else{
+            $(".navbar").removeClass("scrolledNavbar");
         }
-    }
+    });
 });
