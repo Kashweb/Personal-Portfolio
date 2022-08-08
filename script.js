c8c84ef6a6a13927872f050eb0d7b668876512a2
Kashish Chawla <69769135+kashishchawla1@users.noreply.github.com>
2022-08-08 20:43:29 +0930

    Google maos using JS

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ $(".menu-bars").click(function(){
 
 
 });
+
+-12.446872261024101, 130.83924018435545
+-12.455246192756226, 130.84019531546517
+-12.455004230327784, 130.8421776692171
+
+// Initialize and add the map
+function initMap() {
+    const whereILive = { lat: -12.455004230327784, lng: 130.8421776692171 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 15,
+      center: whereILive,
+    });
+  }
+  
+  window.initMap = initMap;
